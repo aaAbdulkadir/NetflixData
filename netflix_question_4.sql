945427043c5e1432871517d3ceb8f5e85bcdaf1b
aaAbdulkadir <72317571+aaAbdulkadir@users.noreply.github.com>
2022-02-28 16:46:48 +0000

    Update netflix_question_4.sql

--- netflix_question_4.sql
+++ netflix_question_4.sql
@@ -1,20 +1,20 @@
--- question 4:•	How many Cast Members in Hükümet Kadin
---SELECT title, COUNT(cast) as count
---FROM Netflix.dbo.master_sheet_netflix, Netflix.dbo.[netflix_titles_cast - netflix_titles_cast]
---WHERE Netflix.dbo.master_sheet_netflix.show_id = Netflix.dbo.[netflix_titles_cast - netflix_titles_cast].show_id and title = 'Hükümet Kadin'
---GROUP BY title
+-- question 4:â€¢	How many Cast Members in HÃ¼kÃ¼met Kadin
+SELECT title, COUNT(cast) as count
+FROM Netflix.dbo.master_sheet_netflix, Netflix.dbo.[netflix_titles_cast - netflix_titles_cast]
+WHERE Netflix.dbo.master_sheet_netflix.show_id = Netflix.dbo.[netflix_titles_cast - netflix_titles_cast].show_id and title = 'HÃ¼kÃ¼met Kadin'
+GROUP BY title
 
---SELECT title, cast as cast_member
---FROM Netflix.dbo.master_sheet_netflix, Netflix.dbo.[netflix_titles_cast - netflix_titles_cast]
---WHERE Netflix.dbo.master_sheet_netflix.show_id = Netflix.dbo.[netflix_titles_cast - netflix_titles_cast].show_id and title = 'Hükümet Kadin'
---GROUP BY title, cast
+SELECT title, cast as cast_member
+FROM Netflix.dbo.master_sheet_netflix, Netflix.dbo.[netflix_titles_cast - netflix_titles_cast]
+WHERE Netflix.dbo.master_sheet_netflix.show_id = Netflix.dbo.[netflix_titles_cast - netflix_titles_cast].show_id and title = 'HÃ¼kÃ¼met Kadin'
+GROUP BY title, cast
 
---CREATE TABLE Netflix.dbo.question_4(
---	title NVARCHAR(MAX),
---	cast_member NVARCHAR(MAX))
+CREATE TABLE Netflix.dbo.question_4(
+	title NVARCHAR(MAX),
+	cast_member NVARCHAR(MAX))
 
 INSERT INTO Netflix.dbo.question_4(title, cast_member)
 SELECT title, cast as cast_member
 FROM Netflix.dbo.master_sheet_netflix, Netflix.dbo.[netflix_titles_cast - netflix_titles_cast]
-WHERE Netflix.dbo.master_sheet_netflix.show_id = Netflix.dbo.[netflix_titles_cast - netflix_titles_cast].show_id and title = 'Hükümet Kadin'
+WHERE Netflix.dbo.master_sheet_netflix.show_id = Netflix.dbo.[netflix_titles_cast - netflix_titles_cast].show_id and title = 'HÃ¼kÃ¼met Kadin'
 GROUP BY title, cast
