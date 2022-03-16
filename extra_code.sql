59a433e654decf6d107a7a7f339f1ddeffeb3daa
aaAbdulkadir <72317571+aaAbdulkadir@users.noreply.github.com>
2022-03-16 16:01:27 +0000

    Update extra_code.sql

--- extra_code.sql
+++ extra_code.sql
@@ -3,3 +3,11 @@
   INNER JOIN [dbo].[master_sheet_netflix] B ON A.show_id = B.show_id
   INNER JOIN [Netflix].[dbo].[netflix_titles_countries - netflix_titles_countries] C ON A.show_id = C.show_id
   GROUP BY A.listed_in
+  
+  OR
+  
+  SELECT Category.listed_in, COUNT(Master.type) AS TypeCount, COUNT(Countries.country) AS CountryCount
+  FROM Netflix.[dbo].[netflix_titles_category - netflix_titles_category] Category
+  JOIN Netflix.[dbo].[master_sheet_netflix] Master ON Category.show_id = Master.show_id
+  JOIN Netflix.[dbo].[netflix_titles_countries - netflix_titles_countries] Countries ON Category.show_id = Countries.show_id
+  GROUP BY Category.listed_in
